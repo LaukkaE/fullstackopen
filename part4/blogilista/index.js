2886165663b94d14b86b27cf43b4a3f55b0084df
LaukkaE <erkkalaukkanen@gmail.com>
2021-08-04 20:33:44 +0300

    blogilista-alku

---
+++ part4/blogilista/index.js
@@ -0,0 +1,49 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number,
+});
+
+const Blog = mongoose.model('Blog', blogSchema);
+
+mongoose
+    .connect(config.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('connected to MongoDB');
+    })
+    .catch((error) => {
+        console.log('error connection to MongoDB:', error.message);
+    });
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/blogs', (request, response) => {
+    Blog.find({}).then((blogs) => {
+        response.json(blogs);
+    });
+});
+
+app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body);
+
+    blog.save().then((result) => {
+        response.status(201).json(result);
+    });
+});
+
+const PORT = config.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
