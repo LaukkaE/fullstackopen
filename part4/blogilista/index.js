a90c63cb8d3868b32f3a71709b35880884ad61e8
LaukkaE <erkkalaukkanen@gmail.com>
2021-08-05 01:03:57 +0300

    4.2step2 done

--- part4/blogilista/index.js
+++ part4/blogilista/index.js
@@ -2,17 +2,11 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const mongoose = require('mongoose');
 const config = require('./utils/config');
+const blogsRouter = require('./controllers/blogs');
+const mongoose = require('mongoose');
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number,
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
+// const Blog = mongoose.model('Blog', blogSchema);
 
 mongoose
     .connect(config.MONGODB_URI, {
@@ -28,20 +22,7 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-    Blog.find({}).then((blogs) => {
-        response.json(blogs);
-    });
-});
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body);
-
-    blog.save().then((result) => {
-        response.status(201).json(result);
-    });
-});
+app.use('/api/blogs', blogsRouter);
 
 const PORT = config.PORT || 3001;
 app.listen(PORT, () => {
