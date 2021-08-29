330cb031b95274d62e4ab356af1301dfe79a2e61
LaukkaE <erkkalaukkanen@gmail.com>
2021-08-30 01:27:37 +0300

    6.b done

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,32 +1,46 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { voteAnecdote } from '../reducers/anecdoteReducer';
+import { setNotification } from '../reducers/notificationReducer';
 
 const AnecdoteList = () => {
+    const [timer, setTimer] = useState(null);
     const [sortedAnecdotes, setSortedAnecdotes] = useState([]);
     const dispatch = useDispatch();
-    const anecdotes = useSelector((state) => state);
+    const anecdotes = useSelector((state) => state.anecdotes);
+    const filter = useSelector((state) => state.filter);
 
     //redux storen sorttaamiseen varmaan parempikin tapa kun tehdä se komponentin sisällä uuteen muuttujaan, mutta en nyt pikaisella googlauksella löytänyt hyvää tapaa.
     useEffect(() => {
         setSortedAnecdotes(anecdotes.sort((a, b) => b.votes - a.votes));
     }, [anecdotes]);
+    const handleClick = (anecdote) => {
+        clearTimeout(timer);
+        dispatch(voteAnecdote(anecdote.id));
+        dispatch(setNotification(`you voted "${anecdote.content}"`));
+        // tallennetaan timeout stateen, jossa se yliajetaan ennen timeoutin payloadin runnaamista jos kahta objectia votetaan viiden sekunnin sisään.
+        setTimer(
+            setTimeout(() => {
+                dispatch(setNotification(''));
+            }, 5000)
+        );
+    };
     return (
         <div>
             <h2>Saved Anecdotes</h2>
-            {sortedAnecdotes.map((anecdote) => (
-                <div key={anecdote.id}>
-                    <div>{anecdote.content}</div>
-                    <div>
-                        has {anecdote.votes}
-                        <button
-                            onClick={() => dispatch(voteAnecdote(anecdote.id))}
-                        >
-                            vote
-                        </button>
+            {sortedAnecdotes
+                .filter((anecdote) => anecdote.content.includes(filter))
+                .map((anecdote) => (
+                    <div key={anecdote.id}>
+                        <div>{anecdote.content}</div>
+                        <div>
+                            has {anecdote.votes}
+                            <button onClick={() => handleClick(anecdote)}>
+                                vote
+                            </button>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))}
         </div>
     );
 };
